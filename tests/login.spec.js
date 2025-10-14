6183d0b1fc565cc8edb26e498bb7a89a658ad18f
ornops <joshuaornopia@gmail.com>
2025-10-14 16:33:14 +0800

    Updated Login spec file

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,5 +3,10 @@ import { test, expect } from '@playwright/test'
 
 test.only('This is a login test', async ({ page }) => {
 	await page.goto('https://www.saucedemo.com/');
+	await page.locator('#user-name').click();
+	await page.locator('#user-name').fill('standard_user')
+	await page.locator('#password').click();
+	await page.locator('#password').fill('secret_sauce');
+	await page.getByRole('button', { name: /login/i }).click();
 	await page.pause();
 })
