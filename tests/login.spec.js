224ed52b2891b743182fd5bca22d038d2b1944b5
ornops <joshuaornopia@gmail.com>
2025-10-15 14:21:18 +0800

    Updated login spec file

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,16 @@
 import { test, expect } from '@playwright/test'
 
 
-test.only('This is a login test', async ({ page }) => {
+test.only('This is a login/logout test', async ({ page }) => {
 	await page.goto('https://www.saucedemo.com/');
 	await page.locator('#user-name').click();
 	await page.locator('#user-name').fill('standard_user')
 	await page.locator('#password').click();
 	await page.locator('#password').fill('secret_sauce');
 	await page.getByRole('button', { name: /login/i }).click();
+	await expect(page.getByText('Swag Labs')).toBeVisible();
+	await page.getByRole('button', { name: 'Open Menu' }).click()
+	await page.getByRole('link', { name: 'Logout' }).click();
+
 	await page.pause();
 })
