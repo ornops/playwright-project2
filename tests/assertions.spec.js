d226c818a655a84fbc82a838a1b650dd31b8ac99
ornops <joshuaornopia@gmail.com>
2025-10-16 22:39:31 +0800

    Updated Assertions spec file

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -1,14 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test.describe.only('Assertion Samples', () => {
+test.describe('Assertion Samples', () => {
 	test('', async ({ page }) => {
 		await page.goto('https://www.saucedemo.com/');
 
 		// Different Assertions
 
-		// To have URL
-		await expect(page).toHaveURL('https://www.saucedemo.com/')
+		// Verify URL
+		await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+		// Verify Title
+		await expect(page).toHaveTitle('Swag Labs');
+		await page.pause();
+	});
+
+	test.only('More Assertions', async ({ page }) => {
+		await page.goto('https://www.saucedemo.com/');
+		// Assert Visibility
+		await expect(page.locator('form')).toBeVisible();
+
+		// Assert element to have text
+		await expect(page.locator('h4').first()).toHaveText('Accepted usernames are:');
 
 		await page.pause();
-	})
-})
+	});
+});
